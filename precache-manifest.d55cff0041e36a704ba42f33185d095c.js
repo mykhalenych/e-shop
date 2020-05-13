a0d036e1adf404bfdca6915437c3aa97f8f52a12
Mykhalenych <mykhalenych@gmail.com>
2020-05-13 09:04:45 +0300

    Updates

---
+++ precache-manifest.d55cff0041e36a704ba42f33185d095c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45543900b5cb1824ef69fdec1f1d8287",
+    "url": "/e-shop/index.html"
+  },
+  {
+    "revision": "e6387b801d2c1b34c671",
+    "url": "/e-shop/static/css/main.a3d3f996.chunk.css"
+  },
+  {
+    "revision": "ce6fc21d0912853d2e0d",
+    "url": "/e-shop/static/js/2.ff60fd3a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/e-shop/static/js/2.ff60fd3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6387b801d2c1b34c671",
+    "url": "/e-shop/static/js/main.4331e750.chunk.js"
+  },
+  {
+    "revision": "8e85185b357e2b99edf6",
+    "url": "/e-shop/static/js/runtime-main.1e2ce722.js"
+  }
+]);
